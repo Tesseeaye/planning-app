2e8508cb502c47b25df93b5eddb473ed374b6a81
Tesseeaye <terryfjunejr@pm.me>
2024-05-23 11:32:49 -0400

    Updated factory names to match their model names.

---
+++ database/factories/CardFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Lists;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Card>
+ */
+class CardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->word(),
+            'content' => fake()->text(300),
+            'lists_id' => Lists::factory(),
+            'user_id' => User::factory(),
+            'position' => fake()->unique()->randomNumber(),
+        ];
+    }
+}
